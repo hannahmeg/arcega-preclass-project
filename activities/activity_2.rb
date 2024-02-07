9b77b28fbb8ced2a677c56df7defd2b404cf68bc
Hannah Meg <hannah_arcega@yahoo.com>
2024-02-07 16:34:51 +0800

    modifications

--- activities/activity_2.rb
+++ activities/activity_2.rb
@@ -3,3 +3,13 @@
 #  Values will be your respective names.
 #  Write a function that takes a Hash and returns the full name.
 #  Print on console the result.
+
+names = { first_name: "Hannah Meg", middle_name: "Salonga", last_name: "Arcega" }
+def get_fullname(given_name)
+
+
+  puts "#{given_name[:first_name]} #{given_name[:middle_name]} #{given_name[:last_name]}"
+
+end
+
+p get_fullname(names)
